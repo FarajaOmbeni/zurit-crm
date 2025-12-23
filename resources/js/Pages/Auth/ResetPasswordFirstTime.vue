584b6a418831356f85968994d685f930be742af2
Ombeni Faraja <mwenzangu.ombeni@strathmore.edu>
2025-12-23 23:19:38 +0300

    allowed users to toggle to view their password

--- resources/js/Pages/Auth/ResetPasswordFirstTime.vue
+++ resources/js/Pages/Auth/ResetPasswordFirstTime.vue
@@ -5,11 +5,23 @@ import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { Head, useForm } from '@inertiajs/vue3';
+import { ref } from 'vue';
 
 const form = useForm({
     password: '',
     password_confirmation: '',
 });
+
+const showPassword = ref(false);
+const showPasswordConfirmation = ref(false);
+
+const togglePasswordVisibility = () => {
+    showPassword.value = !showPassword.value;
+};
+
+const togglePasswordConfirmationVisibility = () => {
+    showPasswordConfirmation.value = !showPasswordConfirmation.value;
+};
 
 const submit = () => {
     form.post(route('password.set.store'), {
@@ -37,15 +49,33 @@ const submit = () => {
                             <div>
                                 <InputLabel for="password" value="New Password" />
 
-                                <TextInput
-                                    id="password"
-                                    type="password"
-                                    class="mt-1 block w-full"
-                                    v-model="form.password"
-                                    required
-                                    autofocus
-                                    autocomplete="new-password"
-                                />
+                                <div class="relative mt-1">
+                                    <TextInput
+                                        id="password"
+                                        :type="showPassword ? 'text' : 'password'"
+                                        class="block w-full pr-12"
+                                        v-model="form.password"
+                                        required
+                                        autofocus
+                                        autocomplete="new-password"
+                                    />
+                                    <button type="button" @click="togglePasswordVisibility"
+                                        class="absolute right-3 top-1/2 -translate-y-1/2 text-zurit-gray hover:text-zurit-purple focus:outline-none focus:text-zurit-purple transition-colors"
+                                        :aria-label="showPassword ? 'Hide password' : 'Show password'">
+                                        <svg v-if="showPassword" class="w-5 h-5" fill="none" stroke="currentColor"
+                                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.736m0 0L21 21" />
+                                        </svg>
+                                        <svg v-else class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                        </svg>
+                                    </button>
+                                </div>
 
                                 <InputError class="mt-2" :message="form.errors.password" />
                             </div>
@@ -56,14 +86,32 @@ const submit = () => {
                                     value="Confirm Password"
                                 />
 
-                                <TextInput
-                                    id="password_confirmation"
-                                    type="password"
-                                    class="mt-1 block w-full"
-                                    v-model="form.password_confirmation"
-                                    required
-                                    autocomplete="new-password"
-                                />
+                                <div class="relative mt-1">
+                                    <TextInput
+                                        id="password_confirmation"
+                                        :type="showPasswordConfirmation ? 'text' : 'password'"
+                                        class="block w-full pr-12"
+                                        v-model="form.password_confirmation"
+                                        required
+                                        autocomplete="new-password"
+                                    />
+                                    <button type="button" @click="togglePasswordConfirmationVisibility"
+                                        class="absolute right-3 top-1/2 -translate-y-1/2 text-zurit-gray hover:text-zurit-purple focus:outline-none focus:text-zurit-purple transition-colors"
+                                        :aria-label="showPasswordConfirmation ? 'Hide password' : 'Show password'">
+                                        <svg v-if="showPasswordConfirmation" class="w-5 h-5" fill="none" stroke="currentColor"
+                                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.736m0 0L21 21" />
+                                        </svg>
+                                        <svg v-else class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                        </svg>
+                                    </button>
+                                </div>
 
                                 <InputError
                                     class="mt-2"
